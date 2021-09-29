bebf6cd825a9c8aad5fbb0995087783625aa3b24
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-09-29 13:39:29 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ListingTransactions } from './src/screens/ListingTransactions';
+
+export default function App() {
+  return (
+    <ListingTransactions />
+  );
+}
