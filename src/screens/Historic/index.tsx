6479b0829dd9c96b60f6750518160647128ca8d7
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-10-03 14:10:27 -0300

    Add graphyc in the Historic

--- src/screens/Historic/index.tsx
+++ src/screens/Historic/index.tsx
@@ -1,6 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { ActivityIndicator } from 'react-native'
 import { StatusBar } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { VictoryPie } from 'victory-native'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { useTheme } from 'styled-components'
+import { useFocusEffect } from '@react-navigation/native'
+import { addMonths, subMonths, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 import { Header } from '../../components/Header'
 import { HistoryCard } from '../../components/HistoryCard'
@@ -10,7 +17,15 @@ import {
   Container,
   HeaderContainer,
   HeaderTitle,
-  HistoryCardContainer
+  HistoryCardContainer,
+  Content,
+  HistoricTitle,
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  SelectIcon,
+  Month,
+  LoadContainer
 } from './styles'
 
 export interface TransactionDataProps {
@@ -24,19 +39,44 @@ export interface TransactionDataProps {
 
 interface CategoryData {
   name: string
-  total: string
+  total: number
+  totalFormatted: string
   color: string
+  percent: string
 }
 
 export function Historic() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [selectedDate, setSelectedDate] = useState(new Date())
   const [totalByCategory, setTotalByCategory] = useState<CategoryData[]>([])
+
+  const theme = useTheme()
+
+  function handleDateChange(action: 'prev' | 'next') {
+    setIsLoading(true)
+
+    if (action === 'next') {
+      setSelectedDate(addMonths(selectedDate, 1))
+
+    } else {
+      setSelectedDate(subMonths(selectedDate, 1))
+    }
+  }
 
   async function loadData() {
     const dataKey = '@myfinances:transactions'
     const response = await AsyncStorage.getItem(dataKey)
     const responseFormatted = response ? JSON.parse(response) : []
 
-    const expensives = responseFormatted.filter((expensive: TransactionDataProps) => expensive.type === 'output')
+    const expensives = responseFormatted.filter((expensive: TransactionDataProps) =>
+      expensive.type === 'output' &&
+      new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
+      new Date(expensive.date).getFullYear() === selectedDate.getFullYear()
+    )
+
+    const expensiveTotal = expensives.reduce((accumulator: number, expensive: TransactionDataProps) => {
+      return accumulator + Number(expensive.amount)
+    }, 0)
 
     const totalByCategory: CategoryData[] = []
 
@@ -50,27 +90,32 @@ export function Historic() {
       })
 
       if (categorySum > 0) {
-        const total = categorySum.toLocaleString('pt-BR', {
+        const totalFormatted = categorySum.toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
         })
 
+        const percent = `${(categorySum / expensiveTotal * 100).toFixed(0)}%`
+
         totalByCategory.push({
           name: category.name,
-          total: total,
-          color: category.color
+          total: categorySum,
+          totalFormatted: totalFormatted,
+          color: category.color,
+          percent: percent
         })
 
       }
     })
 
     setTotalByCategory(totalByCategory)
+    setIsLoading(false)
 
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     loadData()
-  }, [])
+  }, [selectedDate]))
 
   return (
     <Container>
@@ -85,20 +130,70 @@ export function Historic() {
         <HeaderTitle>Histórico de Transações</HeaderTitle>
       </HeaderContainer>
 
-      <HistoryCardContainer>
+      <MonthSelect>
+        <MonthSelectButton onPress={() => handleDateChange('prev')}>
+          <SelectIcon name='chevron-left' />
+        </MonthSelectButton>
 
-      {
-        totalByCategory.map(category => (
-          <HistoryCard
-            key={category.name}
-            title={category.name}
-            amount={category.total}
-            color={category.color}
-          />
+        <Month>
+          {format(selectedDate, 'MMMM, yyyy', { locale: ptBR }).toUpperCase()}
+        </Month>
 
-        ))
+        <MonthSelectButton onPress={() => handleDateChange('next')}>
+          <SelectIcon name='chevron-right' />
+        </MonthSelectButton>
+      </MonthSelect>
+
+      {isLoading
+        ? <LoadContainer>
+          <ActivityIndicator color={theme.colors.attention} size={32} />
+        </LoadContainer>
+
+        : <Content
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 16
+          }}
+        >
+
+
+          <ChartContainer>
+            <VictoryPie
+              data={totalByCategory}
+              x='percent'
+              y='total'
+              colorScale={totalByCategory.map(category => category.color)}
+              style={{
+                labels: {
+                  fontSize: RFValue(14),
+                  fill: theme.colors.title_bold
+                }
+              }}
+              labelRadius={50}
+              width={280}
+              height={280}
+            />
+          </ChartContainer>
+
+
+          <HistoricTitle>Gastos no mês</HistoricTitle>
+          <HistoryCardContainer>
+            {
+              totalByCategory.map(category => (
+                <HistoryCard
+                  key={category.name}
+                  title={category.name}
+                  amount={category.totalFormatted}
+                  color={category.color}
+                />
+
+              ))
+            }
+          </HistoryCardContainer>
+
+        </Content>
       }
-      </HistoryCardContainer>
+
 
 
     </Container>
--- src/screens/Historic/styles.ts
+++ src/screens/Historic/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components/native"
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize"
+import { BorderlessButton } from "react-native-gesture-handler"
+import { Feather } from '@expo/vector-icons'
 
 export const Container = styled.View`
 flex: 1;
@@ -24,5 +26,50 @@ export const HeaderTitle = styled.Text`
 export const HistoryCardContainer = styled.ScrollView`
   flex: 1;
 
-  padding: 24px;
+  padding: 0 24px;
+  margin-bottom: 4px;
+`
+
+export const Content = styled.ScrollView``
+
+export const ChartContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+`
+
+export const MonthSelect = styled.View`
+  width: 100%;
+  margin-top: 24px;
+  padding: 0 24px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const MonthSelectButton = styled(BorderlessButton)`
+  
+`
+
+export const SelectIcon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+  color: ${({ theme }) => theme.colors.text};
+`
+
+export const Month = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.text};
+`
+
+export const HistoricTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.light};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.title_bold};
+  margin-left: 24px;
+`
+export const LoadContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `
